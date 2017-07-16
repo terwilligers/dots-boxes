95b70bfeba15361a6830f7e54bed79ef2806f50e
Sam Terwilliger <sam_terwilliger@Sams-MacBook-Pro.local>
2017-07-16 15:35:21 -0400

    Now uses double-cross strategy

--- game1.py
+++ game1.py
@@ -25,46 +25,99 @@ def find_move(gameboard, win):
     oneNeighbors = []
     twoNeighbors = []
     for j in range(len(gameboard.board)):
-            row = gameboard.board[j]
-            for i in range(len(row)):
-                #Checks if it is a line
-                if (i % 2 == 0 and j % 2 != 0) or (i % 2 != 0 and j % 2 == 0):
-                    #Checks if it is drawn
-                    if gameboard.draw_values[j][i] == False:
-                        #Checks if drawing it will complete a square
-                        if complete_square(gameboard, win, j, i):
-                            gameboard.draw(win, j, i)
+        row = gameboard.board[j]
+        for i in range(len(row)):
+            #Checks if it is a line
+            if (i % 2 == 0 and j % 2 != 0) or (i % 2 != 0 and j % 2 == 0):
+                #Checks if it is drawn
+                if gameboard.draw_values[j][i] == False:
+                    #Checks if drawing it will complete a square
+                    if complete_square(gameboard, win, j, i):
+                        if double_cross_check(gameboard, win, j, i) and double_cross_play(gameboard, win, j, i):
+                            print("executed double_cross")
                             return
                         else:
-                            localmin = neighbor_count(gameboard, win, j, i)
-                            #This checks if the number of neighbor lines of this line is less than the minimum so far
-                            if localmin < min:
-                                min = localmin
-                                i_min = i
-                                j_min = j
-                            if localmin == 0:
-                                noNeighbors.append([j,i])
-                            elif localmin == 1:
-                                oneNeighbors.append([j,i])
-                            else:
-                                twoNeighbors.append([j,i])
+                            gameboard.draw(win, j, i)
+                            return
+                    else:
+                        localmin = neighbor_count(gameboard, win, j, i)
+                        #This checks if the number of neighbor lines of this line is less than the minimum so far
+                        if localmin < min:
+                            min = localmin
+                            i_min = i
+                            j_min = j
+                        if localmin == 0:
+                            noNeighbors.append([j,i])
+                        elif localmin == 1:
+                            oneNeighbors.append([j,i])
+                        else:
+                            twoNeighbors.append([j,i])
     #for minimum neighbors of 0 or 1 we pick a random place to draw line
     if min == 0:
-        index = random.randint(0,len(noNeighbors))
+        index = random.randint(0,len(noNeighbors)-1)
         j_min = noNeighbors[index][0]
         i_min = noNeighbors[index][1]
     elif min == 1:
-        index = random.randint(0,len(noNeighbors))
+        index = random.randint(0,len(oneNeighbors)-1)
         j_min = oneNeighbors[index][0]
         i_min = oneNeighbors[index][1]
     #Finds move where human completes the least squares when human will complete at least 1
-    if min == 2:
+    elif min == 2:
         optimal = forced_move(gameboard, win, twoNeighbors)
         j_min = optimal[0]
         i_min = optimal[1]
     gameboard.draw(win, j_min, i_min)
     return
 
+def double_cross_check(gameboard, win, j, i):
+    '''Checks to see whether a double_cross move is appropriate
+    '''
+    nextChain = 0
+    gameboard.alt_draw(win, j, i)
+    numComplete = 1 #set to one since we already drew one line in alt_draw list
+    while calc_routes(gameboard, win, False, 0, 0):
+        numComplete += 1
+    #Loop through board and see what the size of largest remaining chain is
+    for j in range(len(gameboard.board)):
+        row = gameboard.board[j]
+        for i in range(len(row)):
+            #Checks if it is a line
+            if (i % 2 == 0 and j % 2 != 0) or (i % 2 != 0 and j % 2 == 0):
+                #Checks if it is drawn
+                if gameboard.draw_values[j][i] == False:
+                    #Checks if it will be 3rd side of square
+                    if neighbor_count(gameboard, win, j, i) == 2:
+                        #checks how long the chain is
+                        gameboard.alt_draw(win, j, i)
+                        length = 0
+                        while calc_routes(gameboard, win, False, 0, 0):
+                            length += 1
+                        if length > nextChain:
+                            nextChain = length
+    gameboard.clear_alts(win)
+    #We want there to be only two boxes left in the chain and the next chain to be greater than 2
+    if nextChain > 2 and numComplete == 2:
+        return True
+    return False
+
+def double_cross_play(gameboard, win, j_check, i_check):
+    for j in range(len(gameboard.board)):
+        row = gameboard.board[j]
+        for i in range(len(row)):
+            #Checks if it is a line
+            if (i % 2 == 0 and j % 2 != 0) or (i % 2 != 0 and j % 2 == 0):
+                #Checks if it is drawn
+                if gameboard.draw_values[j][i] == False:
+                    #Checks if it will be 3rd side of square
+                    if neighbor_count(gameboard, win, j, i) == 2:
+                        #checks if this is current chain
+                        if calc_routes(gameboard, win, True, j, i):
+                            gameboard.draw(win, j, i)
+                            gameboard.clear_alts(win)
+                            return True
+            gameboard.clear_alts(win)
+    gameboard.clear_alts(win)
+    return False
 
 def forced_move(gameboard, win, forced_choices):
     '''loops through the options and selects the one that will give the human
@@ -75,27 +128,39 @@ def forced_move(gameboard, win, forced_choices):
     for pair in forced_choices:
         gameboard.alt_draw(win,pair[0],pair[1])
         numComplete = 0
-        while calc_routes(gameboard, win):
+        while calc_routes(gameboard, win, False, 0, 0):
             numComplete += 1
         if numComplete < minComplete or minComplete < 0:
             minComplete = numComplete
             best_pair = pair
+        #clears the alt_values since the analysis of the board is complete
         gameboard.clear_alts(win)
     return best_pair
 
-def calc_routes(gameboard, win):
+def calc_routes(gameboard, win, cross, cross_j, cross_i):
+    '''This function is ran to see how many boxes a player will be able to
+        complete next if a choice is taken
+        The cross, cross_j, and cross_i parameters are only used when using
+        double cross strategy, otherwise they are set to False, 0, 0
+    '''
     for j in range(len(gameboard.board)):
-            row = gameboard.board[j]
-            for i in range(len(row)):
-                #Checks if it is a line
-                if (i % 2 == 0 and j % 2 != 0) or (i % 2 != 0 and j % 2 == 0):
-                    #Checks if it is drawn
-                    if gameboard.draw_values[j][i] == False and gameboard.alt_values[j][i] == False:
-                        #Checks if drawing it will complete a square
-                        if complete_square(gameboard, win, j, i):
-                            gameboard.alt_draw(win, j, i)
+        row = gameboard.board[j]
+        for i in range(len(row)):
+            #Checks if it is a line
+            if (i % 2 == 0 and j % 2 != 0) or (i % 2 != 0 and j % 2 == 0):
+                #Checks if it is drawn
+                if gameboard.draw_values[j][i] == False and gameboard.alt_values[j][i] == False:
+                    #Checks if drawing it will complete a square
+                    #when double crossing theis means that it was the obvious move
+                    if complete_square(gameboard, win, j, i):
+                        gameboard.alt_draw(win, j, i)
+                        #If double crossing we want to see if the potential double cross spot would
+                        #be able to complete a square if the obvious choice was filled in
+                        if cross and complete_square(gameboard, win, cross_j, cross_i):
                             return True
-
+                        if not cross:
+                            return True
+    return False
 
 def complete_square(gameboard, win, j, i):
     '''This function decides whether a move will complete a square by looping through the list of
